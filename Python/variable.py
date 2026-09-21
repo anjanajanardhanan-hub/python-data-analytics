523b4977ef547744b6c8e565124a3fbe373076d0
Anjana Janardhanan <anjanaa3344@gmail.com>
2026-09-21 11:37:39 +0530

    Add files via upload

---
+++ Python/variable.py
@@ -0,0 +1,11 @@
+# What is a variable?
+# A variable is a name used to store a value in a program.
+
+
+#  Swap two variables without a third variable
+a = 5
+b = 10
+a = a + b
+b = a - b
+a = a - b
+print(a, b)  # 10 5
